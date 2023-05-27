9d4f2b7de8029eabfbbe260797e6ae51da7f7fe8
yanyangxiao <yanyangxiaoxyy@gmail.com>
2023-05-27 18:23:29 +0800

    Delete examples directory

--- examples/flower/run-edge-detection.bat
+++
@@ -1,2 +0,0 @@
-..\..\build\bin\Release\edge-detection.exe flower.jpg 3 30.0 20.0
-pause
--- examples/run-curved-triangulation.bat
+++
@@ -1,2 +0,0 @@
-..\..\build\bin\Release\curved-triangulation.exe 0 0.0 flower.jpg init-mesh.om 2-processed-features.txt 120 10
-pause
