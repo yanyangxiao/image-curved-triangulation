1ee73d4362cfe51b758bb488e7f63bad012c1673
yanyangxiao <yanyangxiaoxyy@gmail.com>
2023-05-27 18:32:31 +0800

    upload examples/cherry

---
+++ examples/cherry/run-curved-triangulation.bat
@@ -0,0 +1,2 @@
+..\..\build\bin\Release\curved-triangulation.exe 1 0.0 cherry.jpg init-mesh.om 2-processed-features.txt 200 10
+pause
---
+++ examples/cherry/run-edge-detection.bat
@@ -0,0 +1,2 @@
+..\..\build\bin\Release\edge-detection.exe cherry.jpg 3 30.0 40.0
+pause
