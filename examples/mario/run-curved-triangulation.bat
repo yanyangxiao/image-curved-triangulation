febfa166b57a3a04ce6bc811ab4a3a9a7c81685c
yanyangxiao <yanyangxiaoxyy@gmail.com>
2023-05-27 18:27:11 +0800

    upload examples/mario

---
+++ examples/mario/run-curved-triangulation.bat
@@ -0,0 +1,2 @@
+..\..\build\bin\Release\curved-triangulation.exe 0 0.0 mario.jpg init-mesh.om 2-processed-features.txt 400 20
+pause
---
+++ examples/mario/run-edge-detection.bat
@@ -0,0 +1,2 @@
+..\..\build\bin\Release\edge-detection.exe mario.jpg 3 30.0 50.0
+pause
